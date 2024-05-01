0509554ceda8813d91f081c6bf1b13b101d6e219
Abdoullatoundji <70619408+Abdoullatoundji@users.noreply.github.com>
2024-05-01 17:11:14 -0500

    Update RegisterForm.js

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,4 +1,5 @@
 // src/components/RegisterForm.js
+
 import React from 'react';
 
 function RegisterForm() {
@@ -6,9 +7,30 @@ function RegisterForm() {
     <div className="center-div">
       <form action="InsertBookDetails" method="post">
         <h3>Enter book details here:</h3>
-        <label htmlFor="booktitle">Book Title:</label>
-        <input type="text" id="booktitle" name="booktitle" placeholder="Lord of the Rings..." />
-        {/* Add other input fields */}
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="booktitle">Book Title:</label>
+            <input type="text" id="booktitle" name="booktitle" placeholder="Lord of the Rings..." />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="author">Author:</label>
+            <input type="text" id="author" name="author" placeholder="J.R.R. Tolkien..." />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="genre">Genre:</label>
+            <input type="text" id="genre" name="genre" placeholder="High Fantasy..." />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="isbn">ISBN:</label>
+            <input type="text" id="isbn" name="isbn" />
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <label htmlFor="summary">Summary:</label>
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+            <textarea name="summary" rows="5" cols="35" placeholder="Summary goes here..."></textarea>
+          </div>
+        </div>
         <input type="submit" />
       </form>
     </div>
@@ -16,4 +38,5 @@ function RegisterForm() {
 }
 
 export default RegisterForm;
+
 
